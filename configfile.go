491be06b53b6ba7db8201629d97f353fc1376d5f
acoshift <acoshift@gmail.com>
2018-05-18 10:03:08 +0700

    add duration

--- configfile.go
+++ configfile.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/acoshift/configfile/internal/reader"
 )
@@ -46,35 +47,26 @@ func (r *Reader) readString(name string) (string, error) {
 }
 
 func (r *Reader) readInt(name string) (int, error) {
-	b, err := r.r.Read(name)
+	s, err := r.readString(name)
 	if err != nil {
 		return 0, err
 	}
-	i, err := strconv.Atoi(string(b))
-	if err != nil {
-		return 0, err
-	}
-	return i, nil
+	return strconv.Atoi(s)
 }
 
 func (r *Reader) readInt64(name string) (int64, error) {
-	b, err := r.r.Read(name)
+	s, err := r.readString(name)
 	if err != nil {
 		return 0, err
 	}
-	i, err := strconv.ParseInt(string(b), 10, 64)
-	if err != nil {
-		return 0, err
-	}
-	return i, nil
+	return strconv.ParseInt(s, 10, 64)
 }
 
 func (r *Reader) readBool(name string) (bool, error) {
-	b, err := r.r.Read(name)
+	s, err := r.readString(name)
 	if err != nil {
 		return false, err
 	}
-	s := string(b)
 	if s == "" {
 		return false, io.EOF
 	}
@@ -85,6 +77,14 @@ func (r *Reader) readBool(name string) (bool, error) {
 		return false, nil
 	}
 	return true, nil
+}
+
+func (r *Reader) readDuration(name string) (time.Duration, error) {
+	s, err := r.readString(name)
+	if err != nil {
+		return 0, err
+	}
+	return time.ParseDuration(s)
 }
 
 // BytesDefault reads bytes from config file with default value
@@ -198,6 +198,30 @@ func (r *Reader) Bool(name string) bool {
 // panic if file not exists
 func (r *Reader) MustBool(name string) bool {
 	b, err := r.readBool(name)
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
+// DurationDefault reads string then parse as duration from config file with default value
+func (r *Reader) DurationDefault(name string, def time.Duration) time.Duration {
+	d, err := r.readDuration(name)
+	if err != nil {
+		return def
+	}
+	return d
+}
+
+// Duration reads string then parse as duration from config file
+func (r *Reader) Duration(name string) time.Duration {
+	return r.DurationDefault(name, 0)
+}
+
+// MustDuration reads string then parse as duration from config file,
+// panic if file not exists
+func (r *Reader) MustDuration(name string) time.Duration {
+	b, err := r.readDuration(name)
 	if err != nil {
 		panic(err)
 	}
